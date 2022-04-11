a7b7b95e7ffa8becb2482cb8ed3f94c58aaede57
BoomPiter <boompiter@gmail.com>
2022-04-11 23:21:28 +0500

    complite all task

--- lesson-2.py
+++ lesson-2.py
@@ -2,73 +2,73 @@
 # каждого элемента. Использовать функцию type() для проверки типа. Элементы списка можно не запрашивать у
 # пользователя, а указать явно, в программе.
 
-# scrl = [12, None, -77, 'True', True, 9.5]
-#
-# def type_scrl():
-#    print(list(map(type, scrl)))
-#    return
-#
-#
-# type_scrl()
+scrl = [12, None, -77, 'True', True, 9.5]
+
+def type_scrl():
+   print(list(map(type, scrl)))
+   return
+
+
+type_scrl()
 
 # 2. Для списка реализовать обмен значений соседних элементов. Значениями обмениваются элементы с индексами 0 и 1,
 # 2 и 3 и т. д. При нечётном количестве элементов последний сохранить на своём var = месте.Для заполнения списка
 # элементов нужно использовать функцию input().
 
-# el_count = int(input("Введите количество элементов списка "))
-# my_list = []
-# i = 0
-# el = 0
-# while i < el_count:
-#     my_list.append(input("Введите следующее значение списка "))
-#     i += 1
-#
-# for elem in range(int(len(my_list) / 2)):
-#     my_list[el], my_list[el + 1] = my_list[el + 1], my_list[el]
-#     el += 2
-# print(my_list)
+el_count = int(input("Введите количество элементов списка "))
+my_list = []
+i = 0
+el = 0
+while i < el_count:
+    my_list.append(input("Введите следующее значение списка "))
+    i += 1
+
+for elem in range(int(len(my_list) / 2)):
+    my_list[el], my_list[el + 1] = my_list[el + 1], my_list[el]
+    el += 2
+print(my_list)
 
 # 3. Пользователь вводит месяц в виде целого числа от 1 до 12. Сообщить, к какому времени года относится месяц (зима,
 # весна, лето, осень). Напишите решения через list и dict.
-#
+
 
 
 
 # 4. Пользователь вводит строку из нескольких слов, разделённых пробелами. Вывести каждое слово с новой строки.
 # Строки нужно пронумеровать. Если слово длинное, выводить только первые 10 букв в слове.
 
-# my_str = input("введите строку ")
-# my_word = []
-# num = 1
-# for el in range(my_str.count(' ') + 1):
-#     my_word = my_str.split()
-#     if len(str(my_word)) <= 10:
-#         print(f" {num} {my_word [el]}")
-#         num += 1
-#     else:
-#         print(f" {num} {my_word [el] [0:10]}")
-#         num += 1
+my_str = input("введите строку ")
+my_word = []
+num = 1
+for el in range(my_str.count(' ') + 1):
+    my_word = my_str.split()
+    if len(str(my_word)) <= 10:
+        print(f" {num} {my_word [el]}")
+        num += 1
+    else:
+        print(f" {num} {my_word [el] [0:10]}")
+        num += 1
 
 # 5. Реализовать структуру «Рейтинг», представляющую собой набор натуральных чисел, который не возрастает. У
 # пользователя нужно запрашивать новый элемент рейтинга. Если в рейтинге существуют элементы с одинаковыми
 # значениями, то новый элемент с тем же значением должен разместиться после них.
 
-# my_list = [7, 5, 3, 3, 2]
-# print(f"Рейтинг - {my_list}")
-# digit = int(input("Введите число (111 - выход) "))
-# while digit != 111:
-#     for el in range(len(my_list)):
-#         if my_list[el] == digit:
-#             my_list.insert(el + 1, digit)
-#             break
-#         elif my_list[0] < digit:
-#             my_list.insert(0, digit)
-#         elif my_list[-1] > digit:
-#             my_list.append(digit)
-#         elif my_list[el] > digit > my_list[el + 1]:
-#             my_list.insert(el + 1, digit)
-#     print(f"текущий список - {my_list}")
-#     digit = int(input("Введите число "))
+my_list = [7, 5, 3, 3, 2]
+print(f"Рейтинг - {my_list}")
+digit = int(input("Введите число (111 - выход) "))
+while digit != 111:
+    for el in range(len(my_list)):
+        if my_list[el] == digit:
+            my_list.insert(el + 1, digit)
+            break
+        elif my_list[0] < digit:
+            my_list.insert(0, digit)
+        elif my_list[-1] > digit:
+            my_list.append(digit)
+        elif my_list[el] > digit > my_list[el + 1]:
+            my_list.insert(el + 1, digit)
+    print(f"текущий список - {my_list}")
+    digit = int(input("Введите число "))
 
 # 6. (Дополнительно) Реализовать структуру данных «Товары». Она должна представлять собой список кортежей. Каждый
 # кортеж хранит информацию об отдельном товаре. В кортеже должно быть два элемента — номер товара и словарь с
